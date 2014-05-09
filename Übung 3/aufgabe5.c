767126eaf8500421ceee088a03e2ce7ea7ca9ef9
shlnz <jens.thom@ruhr-uni-bochum.de>
2014-05-09 13:38:19 +0200

    Aufgabe3 (beta)

---
+++ Übung 3/aufgabe5.c
@@ -0,0 +1,22 @@
+#include <avr/io.h>
+#include <inttypes.h>
+
+int main(void){
+	//Array mit 8 Werten der XOR Summe berechnet werden soll
+	const uint8_t SRAMarray[8]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+
+	uint8_t i;
+	uint8_t sum=0;
+
+	//ToDo: XORieren Sie hier die Werte aus SRAMarray in einer Schleife auf
+	int len = (sizeof(SRAMarray)/sizeof(SRAMarray[0]));
+	for(i = 0; i < len; i++){
+		sum ^= SRAMarray[i];
+	}
+	
+	//Diese Zuweisung verhindert, dass sum durch den Compiler "wegoptimiert" wird
+	PORTB=sum;
+	
+	while(1);
+	return 0;
+}
