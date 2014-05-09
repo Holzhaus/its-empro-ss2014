9971b72303b258c86e6dd9419b5b887e9a0d26ab
Holzhaus <holthuis.jan@googlemail.com>
2014-05-09 15:04:47 +0200

    Aufgabe 8 gelöst

--- Übung 3/aufgabe8.c
+++ Übung 3/aufgabe8.c
@@ -3,14 +3,17 @@
 #include <inttypes.h>
 
 //korrigieren Sie diese Deklaration, so dass das Array im Flash-Speicher und nicht im SRAM des Mikrocontrollers abgelegt wird
-const uint8_t abc[8]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+const uint8_t abc[8] PROGMEM ={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80}; //PROGMEM-Makro hinzugefügt
 
 int main(void){
 	uint8_t i;
 	uint8_t sum=0;
 
 	//ToDo: lesen Sie die Werte aus dem Array im Flashspeicher aus und XORieren Sie die Werte wie in der vorherigen Aufgabe auf
-
+	int len = (sizeof(abc)/sizeof(abc[0])); //Business as usual
+	for(i = 0; i < len; i++){
+		sum ^= pgm_read_byte(&(abc[i])); //abc[i] aus dem Flash laden (anstatt aus dem SRAM)
+	}
 
 	//Diese Zuweisung verhindert, dass sum durch den Compiler "wegoptimiert" wird
 	PORTB=sum;
